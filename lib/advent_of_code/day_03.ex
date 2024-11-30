c01946c5ba223ee9d32477e567b7073b994203f8
Kasper Vaessen <k.e.vaessen@student.tudelft.nl>
2024-11-30 15:48:14 +0100

    day 3 and 4

--- lib/advent_of_code/day_03.ex
+++ lib/advent_of_code/day_03.ex
@@ -1,7 +1,46 @@
 defmodule AdventOfCode.Day03 do
-  def part1(_args) do
+  alias AdventOfCode.Grid
+
+  def part1(args) do
+    grid = Grid.read_grid(args)
+
+    {gamma, epsilon} =
+      0..(grid.cols - 1)
+      |> Enum.map(&Enum.frequencies(Grid.get_col(grid, &1)))
+      |> Enum.reduce(
+        {"", ""},
+        fn %{"0" => counts0, "1" => counts1}, {gamma, epsilon} ->
+          if counts0 > counts1 do
+            {gamma <> "0", epsilon <> "1"}
+          else
+            {gamma <> "1", epsilon <> "0"}
+          end
+        end
+      )
+
+    String.to_integer(gamma, 2) * String.to_integer(epsilon, 2)
   end
 
-  def part2(_args) do
+  defp find_value(grid, col, cmp_func) do
+    if grid.rows == 1 do
+      Grid.get_row(grid, 0) |> Enum.join() |> String.to_integer(2)
+    else
+      %{"0" => counts0, "1" => counts1} = Enum.frequencies(Grid.get_col(grid, col))
+
+      new_grid =
+        if cmp_func.(counts0, counts1) do
+          Grid.filter_rows(grid, &(elem(&1, col) == "0"))
+        else
+          Grid.filter_rows(grid, &(elem(&1, col) == "1"))
+        end
+
+      find_value(new_grid, col + 1, cmp_func)
+    end
+  end
+
+  def part2(args) do
+    grid = Grid.read_grid(args)
+
+    find_value(grid, 0, &(&1 > &2)) * find_value(grid, 0, &(&1 <= &2))
   end
 end
--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,7 +1,97 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  alias AdventOfCode.Grid
+
+  def part1(args) do
+    [numbers | boards] = args |> String.split(["\n\n", "\r\r", "\r\n\r\n"], trim: true)
+
+    numbers = String.split(numbers, ",", trim: true) |> Enum.map(&String.to_integer/1)
+    boards = Enum.map(boards, fn board -> Grid.read_grid(board, " ", &String.to_integer/1) end)
+
+    Enum.reduce_while(numbers, boards, fn num, boards ->
+      new_boards =
+        Enum.map(boards, fn board ->
+          Grid.map(
+            board,
+            &if num != &1 do
+              &1
+            end
+          )
+        end)
+
+      case check_win(new_boards, num) do
+        :no_win ->
+          {:cont, new_boards}
+
+        {:win, score, _} ->
+          {:halt, score}
+      end
+    end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    [numbers | boards] = args |> String.split(["\n\n", "\r\r", "\r\n\r\n"], trim: true)
+
+    numbers = String.split(numbers, ",", trim: true) |> Enum.map(&String.to_integer/1)
+    boards = Enum.map(boards, fn board -> Grid.read_grid(board, " ", &String.to_integer/1) end)
+
+    Enum.reduce_while(numbers, boards, fn num, boards ->
+      new_boards =
+        Enum.map(boards, fn board ->
+          Grid.map(
+            board,
+            &if num != &1 do
+              &1
+            end
+          )
+        end)
+
+      case check_win(new_boards, num) do
+        :no_win ->
+          {:cont, new_boards}
+
+        {:win, score, remaining_boards} ->
+          if Enum.empty?(remaining_boards) do
+            {:halt, score}
+          else
+            {:cont, remaining_boards}
+          end
+      end
+    end)
+  end
+
+  defp check_win(boards, num) do
+    result =
+      Enum.map(boards, fn board ->
+        rows_filtered =
+          Grid.filter_rows(board, fn row -> Tuple.to_list(row) |> Enum.all?(&is_nil/1) end)
+
+        cols_filtered =
+          Grid.filter_cols(board, fn col -> Tuple.to_list(col) |> Enum.all?(&is_nil/1) end)
+
+        if rows_filtered.rows > 0 or cols_filtered.cols > 0 do
+          sum = board |> Grid.to_iterable() |> Enum.reject(&is_nil/1) |> Enum.sum()
+          {:win, sum * num}
+        else
+          board
+        end
+      end)
+
+    remaining_boards =
+      Enum.filter(result, fn
+        {:win, _} -> false
+        _ -> true
+      end)
+
+    win =
+      Enum.filter(result, fn
+        {:win, _} -> true
+        _ -> false
+      end)
+
+    if win == [] do
+      :no_win
+    else
+      {:win, elem(hd(win), 1), remaining_boards}
+    end
   end
 end
